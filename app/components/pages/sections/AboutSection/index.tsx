d52fea2ea7c1840a7d21aa6de3a59fe84013e242
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2025-09-24 17:30:22 -0300

    feat: Added about section texts

--- app/components/pages/sections/AboutSection/index.tsx
+++ app/components/pages/sections/AboutSection/index.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import styles from './styles.module.scss'
+import { DefaultButton } from '@/app/components/commons/DefaultButton'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 export function AboutSection() {
   return (
@@ -18,18 +21,48 @@ export function AboutSection() {
           </h2>
 
           <p>
-            Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet.
+            Olá, me chamo Alessandro Cídney! Sou um desenvolvedor web com quase 4 anos de experiência na criação de websites e interfaces.
           </p>
 
           <p>
-            Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet Lorem inpsum dolor sir amet .
+            Comecei minha carreira no ano de 2021, onde comecei a desenvolver e melhorar websites como estagiário front-end.
           </p>
+
+          <p>
+            Desde então, participei de diversos projetos, atuando na construção de interfaces, criação de sistemas de cadastro, demonstrações para empresas,
+            manutenção de APIs e desenvolvimento de pacotes para utilização em aplicações.
+          </p>
+
+          <p>
+            Minha missão é fazer as empresas e clientes atingirem seus objetivos e sonhos por meio da construção de interfaces incríveis
+            que sejam úteis e eficientes para seu negócio.
+          </p>
+
+          <p>
+            Que tal trabalharmos juntos para levarmos a sua empresa ou ferramenta ao próximo nível?
+          </p>
+
+          <div>
+            <DefaultButton
+              appendIcon={<FontAwesomeIcon icon={faArrowUpRightFromSquare} />}
+              href={process.env.NEXT_PUBLIC_LINKEDIN_URL}
+              target='_blank'
+              variant='flat'
+              style={{
+                minWidth: '300px'
+              }}
+              link
+            >
+              Mande uma mensagem
+            </DefaultButton>
+          </div>
         </div>
 
         <div className={styles.imageContainer}>
           <Image
             src='/images/photos/selfie.png'
             alt='Uma fotografia minha.'
+            objectFit='cover'
             fill
           />
         </div>
